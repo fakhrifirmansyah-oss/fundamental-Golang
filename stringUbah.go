71296edb045f9ac20463061c1c275f26f5c059c3
fakhrifirmansyah-oss <fakhrifirmansyah2025@gmail.com>
2026-05-12 23:16:14 +0700

    update semua file

---
+++ stringUbah.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Muhammad Fakhri Firmansyah"))
+	fmt.Println("Muhammad Fakhri Firmansyah"[0])
+	fmt.Println("Muhammad Fakhri Firmansyah"[7])
+}
---
+++ variabel.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var Nama string
+	Nama = "Muhammad Fakhri Firmansyah"
+	fmt.Println(Nama)
+
+	Nama = "Fakhri"
+	fmt.Println(Nama)
+
+	Nama = "Muhammad Fakhri"
+	fmt.Println(Nama)
+	
+}
